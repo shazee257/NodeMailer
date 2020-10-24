c14bd6bd88f62b4c1a2fc2876b768619683bb4f5
Muhammad Shahzad Bhatti <muhammad.2941906@talmeez.pk>
2020-10-24 23:46:46 +0500

    Completed

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const exphbs = require('express-handlebars');
-const path = require('path');
-const nodemailer = require('nodemailer');
+const express = require("express");
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
+const path = require("path");
+const nodemailer = require("nodemailer");
+
+// .env
+require("dotenv").config();
 
 const app = express();
 
 // View engine setup
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: false,
+  })
+);
+app.set("view engine", "handlebars");
 
 // Static folder
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 // Body Parser Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.render('contact');
+app.get("/", (req, res) => {
+  res.render("contact");
 });
 
-app.post('/send', (req, res) => {
+app.post("/send", (req, res) => {
   const output = `
     <p>You have a new contact request</p>
     <h3>Contact Details</h3>
@@ -37,37 +45,37 @@ app.post('/send', (req, res) => {
 
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
-    host: 'mail.YOURDOMAIN.com',
+    host: "smtp.gmail.com",
     port: 587,
     secure: false, // true for 465, false for other ports
     auth: {
-        user: 'YOUREMAIL', // generated ethereal user
-        pass: 'YOURPASSWORD'  // generated ethereal password
+      user: process.env.AuthUser, //  user
+      pass: process.env.AuthPass, //  password
     },
-    tls:{
-      rejectUnauthorized:false
-    }
+    tls: {
+      rejectUnauthorized: false,
+    },
   });
 
   // setup email data with unicode symbols
   let mailOptions = {
-      from: '"Nodemailer Contact" <your@email.com>', // sender address
-      to: 'RECEIVEREMAILS', // list of receivers
-      subject: 'Node Contact Request', // Subject line
-      text: 'Hello world?', // plain text body
-      html: output // html body
+    from: `"Nodemailer Testing" <${process.env.AuthUser}>`, // sender address
+    to: process.env.ReceiverEmail, // list of receivers, may be more than one
+    subject: "NodeMailer Testing", // Subject line
+    text: "Hello world?", // plain text body
+    html: output, // html body
   };
 
   // send mail with defined transport object
   transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-          return console.log(error);
-      }
-      console.log('Message sent: %s', info.messageId);   
-      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 
-      res.render('contact', {msg:'Email has been sent'});
+    res.render("contact", { msg: "Email has been sent" });
   });
-  });
+});
 
-app.listen(3000, () => console.log('Server started...'));
+app.listen(4000, () => console.log("Server started..."));
